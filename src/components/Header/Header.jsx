4b8bc6f2bcfdd1a3b25f9d340abe235d084d61f6
mohdbinharun56 <arahaman461@gmail.com>
2024-10-01 15:45:19 +0600

    implement header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
+import profile from '../../../public/images/profile.png'
+
 const Header = () => {
     return (
-        <div>
-            <h1 className=''> Knowledge Cafe</h1>
-        </div>
+        <header className='md:flex justify-between items-center mx-60 border-b-2 mt-5'>
+            <h1 className='text-4xl font-bold'> Knowledge Cafe</h1>
+            <img src={profile} alt="Profile" />
+        </header>
     );
 };
 
